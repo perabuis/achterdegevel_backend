f752537fcad8537bf72384cb9deddec4889dc530
KaatClinckemaillie <kaat.clinckemaillie@student.howest.be>
2022-06-12 12:47:05 +0200

    add cloudinary

---
+++ src/api/gevel/controllers/gevel.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  gevel controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::gevel.gevel');
---
+++ src/api/gevel/routes/gevel.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * gevel router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::gevel.gevel');
